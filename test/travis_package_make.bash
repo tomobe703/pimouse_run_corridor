cfc9b7701961c1f7b666c425ec77cb9d743f62b3
H.T <tomobe703@gmail.com>
2018-04-15 14:47:10 +0000

    set launch

---
+++ test/travis_package_make.bash
@@ -0,0 +1,11 @@
+#!/bin/bash -xve
+
+#sync and make
+rsync -av ./ ~/catkin_ws/src/pimouse_run_corridor/
+
+cd ~/catkin_ws/src/
+git clone --depth=1 https://github.com/tomobe703/pimouse_ros.git
+
+cd ~/catkin_ws
+catkin_make
+
